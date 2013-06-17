240d62a750c3b1bb1f4b206e6d52c10f9cb2c078
Jonathan Commins <joncom@gmail.com>
2013-06-17 15:14:50 -0700

    Converted tabs to spaces.

--- b2entity.js
+++ b2entity.js
@@ -1,85 +1,85 @@
 ig.module(
-	'plugins.box2d.b2entity'
+    'plugins.box2d.b2entity'
 )
 .requires(
-	'impact.entity',
-	'plugins.box2d.game'
+    'impact.entity',
+    'plugins.box2d.game'
 )
 .defines(function(){
 
     ig.Box2DEntity = ig.Entity.extend({
-	body: null,
-	angle: 0,
+    body: null,
+    angle: 0,
 
-	init: function( x, y , settings ) {
-	    this.parent( x, y, settings );
+    init: function( x, y , settings ) {
+        this.parent( x, y, settings );
 
-	    // Only create a box2d body when we are not in Weltmeister
-	    if( !ig.global.wm ) {
-		this.createBody();
-	    }
-	},
+        // Only create a box2d body when we are not in Weltmeister
+        if( !ig.global.wm ) {
+        this.createBody();
+        }
+    },
 
-	createBody: function() {
-	    var bodyDef = new b2.BodyDef();
-	    bodyDef.position.Set(
-		(this.pos.x + this.size.x / 2) * b2.SCALE,
-		(this.pos.y + this.size.y / 2) * b2.SCALE
-	    );
+    createBody: function() {
+        var bodyDef = new b2.BodyDef();
+        bodyDef.position.Set(
+        (this.pos.x + this.size.x / 2) * b2.SCALE,
+        (this.pos.y + this.size.y / 2) * b2.SCALE
+        );
 
-	    bodyDef.type = b2.Body.b2_dynamicBody;
+        bodyDef.type = b2.Body.b2_dynamicBody;
 
-	    this.body = ig.world.CreateBody(bodyDef);
+        this.body = ig.world.CreateBody(bodyDef);
 
-	    var shapeDef = new b2.PolygonShape();
-	    shapeDef.SetAsBox(
-		this.size.x / 2 * b2.SCALE,
-		this.size.y / 2 * b2.SCALE
-	    );
+        var shapeDef = new b2.PolygonShape();
+        shapeDef.SetAsBox(
+        this.size.x / 2 * b2.SCALE,
+        this.size.y / 2 * b2.SCALE
+        );
 
-	    // CreateFixture2( var shapedef, density )
-	    this.body.CreateFixture2(shapeDef, 0.1);
-	},
+        // CreateFixture2( var shapedef, density )
+        this.body.CreateFixture2(shapeDef, 0.1);
+    },
 
-	// Helpful to handle generation of random spawn point when
-	// object is out of canvas in next frame
-	randomPos: function( body ) {
-	    // Range of acceptable values is: 0.8 > x < 47.2 & 1.6 > y < 26.4
-	    // generate random location
-	    var randPos = {x: Math.random()*45 + 1, y: Math.random()*23 + 2};
+    // Helpful to handle generation of random spawn point when
+    // object is out of canvas in next frame
+    randomPos: function( body ) {
+        // Range of acceptable values is: 0.8 > x < 47.2 & 1.6 > y < 26.4
+        // generate random location
+        var randPos = {x: Math.random()*45 + 1, y: Math.random()*23 + 2};
 
-	    // Set random location
-	    this.body.SetPosition(randPos);
-	},
+        // Set random location
+        this.body.SetPosition(randPos);
+    },
 
 
-	randomVel: function ( body ) {
-	    // Gen +/- for vector
-	    var pOmX = Math.random() < 0.5 ? -1 : 1;
-	    var pOmY = Math.random() < 0.5 ? -1 : 1;
+    randomVel: function ( body ) {
+        // Gen +/- for vector
+        var pOmX = Math.random() < 0.5 ? -1 : 1;
+        var pOmY = Math.random() < 0.5 ? -1 : 1;
 
-	    // gen and set new velocity
-	    this.body.SetLinearVelocity( new b2.Vec2(pOmX*Math.random()*16, pOmY*Math.random()*16));
-	},
+        // gen and set new velocity
+        this.body.SetLinearVelocity( new b2.Vec2(pOmX*Math.random()*16, pOmY*Math.random()*16));
+    },
 
-	update: function() {
-	    var p = this.body.GetPosition();
-	    this.pos = {
-		x: (p.x / b2.SCALE - this.size.x / 2),
-		y: (p.y / b2.SCALE - this.size.y / 2 )
-	    };
-	    this.angle = this.body.GetAngle().round(2);
+    update: function() {
+        var p = this.body.GetPosition();
+        this.pos = {
+        x: (p.x / b2.SCALE - this.size.x / 2),
+        y: (p.y / b2.SCALE - this.size.y / 2 )
+        };
+        this.angle = this.body.GetAngle().round(2);
 
-	    if( this.currentAnim ) {
-		this.currentAnim.update();
-		this.currentAnim.angle = this.angle;
-	    }
-	},
+        if( this.currentAnim ) {
+        this.currentAnim.update();
+        this.currentAnim.angle = this.angle;
+        }
+    },
 
-	kill: function() {
-	    ig.world.DestroyBody( this.body );
-	    this.parent();
-	}
+    kill: function() {
+        ig.world.DestroyBody( this.body );
+        this.parent();
+    }
     });
 
 });
--- game.js
+++ game.js
@@ -1,9 +1,9 @@
 ig.module(
-	'plugins.box2d.game'
+    'plugins.box2d.game'
 )
 .requires(
-	'plugins.box2d.lib',
-	'impact.game'
+    'plugins.box2d.lib',
+    'impact.game'
 )
 .defines(function(){
 
@@ -11,130 +11,130 @@ ig.module(
 
 ig.Box2DGame = ig.Game.extend({
 
-	collisionRects: [],
-	debugCollisionRects: false,
+    collisionRects: [],
+    debugCollisionRects: false,
 
 
-	loadLevel: function( data ) {
+    loadLevel: function( data ) {
 
-		// Find the collision layer and create the box2d world from it
-		for( var i = 0; i < data.layer.length; i++ ) {
-			var ld = data.layer[i];
-			if( ld.name == 'collision' ) {
-				ig.world = this.createWorldFromMap( ld.data, ld.width, ld.height, ld.tilesize );
-				break;
-			}
-		}
+        // Find the collision layer and create the box2d world from it
+        for( var i = 0; i < data.layer.length; i++ ) {
+            var ld = data.layer[i];
+            if( ld.name == 'collision' ) {
+                ig.world = this.createWorldFromMap( ld.data, ld.width, ld.height, ld.tilesize );
+                break;
+            }
+        }
 
-		this.parent( data );
-	},
-
-
-	createWorldFromMap: function( origData, width, height, tilesize ) {
-		var gravity = new b2.Vec2( 0, this.gravity * b2.SCALE );
-		var world = new b2.World(gravity, true );
+        this.parent( data );
+    },
 
 
-		// We need to delete those tiles that we already processed. The original
-		// map data is copied, so we don't destroy the original.
-		var data = ig.copy( origData );
-
-		// Get all the Collision Rects from the map
-		this.collisionRects = [];
-		for( var y = 0; y < height; y++ ) {
-			for( var x = 0; x < width; x++ ) {
-				// If this tile is solid, find the rect of solid tiles starting
-				// with this one
-				if( data[y][x] ) {
-					var r = this._extractRectFromMap( data, width, height, x, y );
-					this.collisionRects.push( r );
-				}
-			}
-		}
-
-		// Go through all rects we gathered and create Box2D objects from them
-		for( var i = 0; i < this.collisionRects.length; i++ ) {
-			var rect = this.collisionRects[i];
-
-			var bodyDef = new b2.BodyDef();
-			bodyDef.position.Set(
-				rect.x * tilesize * b2.SCALE + rect.width * tilesize / 2 * b2.SCALE,
-				rect.y * tilesize * b2.SCALE + rect.height * tilesize / 2 * b2.SCALE
-			);
-
-			var body = world.CreateBody( bodyDef );
-			var shape = new b2.PolygonShape();
-			shape.SetAsBox(
-				rect.width * tilesize / 2 * b2.SCALE,
-				rect.height * tilesize / 2 * b2.SCALE
-			);
-			body.CreateFixture2( shape );
-		}
-
-		return world;
-	},
+    createWorldFromMap: function( origData, width, height, tilesize ) {
+        var gravity = new b2.Vec2( 0, this.gravity * b2.SCALE );
+        var world = new b2.World(gravity, true );
 
 
-	_extractRectFromMap: function( data, width, height, x, y ) {
-		var rect = {x: x, y: y, width: 1, height: 1};
+        // We need to delete those tiles that we already processed. The original
+        // map data is copied, so we don't destroy the original.
+        var data = ig.copy( origData );
 
-		// Find the width of this rect
-		for(var wx = x + 1; wx < width && data[y][wx]; wx++ ) {
-			rect.width++;
-			data[y][wx] = 0; // unset tile
-		}
+        // Get all the Collision Rects from the map
+        this.collisionRects = [];
+        for( var y = 0; y < height; y++ ) {
+            for( var x = 0; x < width; x++ ) {
+                // If this tile is solid, find the rect of solid tiles starting
+                // with this one
+                if( data[y][x] ) {
+                    var r = this._extractRectFromMap( data, width, height, x, y );
+                    this.collisionRects.push( r );
+                }
+            }
+        }
 
-		// Check if the next row with the same width is also completely solid
-		for( var wy = y + 1; wy < height; wy++ ) {
-			var rowWidth = 0;
-			for( wx = x; wx < x + rect.width && data[wy][wx]; wx++ ) {
-				rowWidth++;
-			}
+        // Go through all rects we gathered and create Box2D objects from them
+        for( var i = 0; i < this.collisionRects.length; i++ ) {
+            var rect = this.collisionRects[i];
 
-			// Same width as the rect? -> All tiles are solid; increase height
-			// of this rect
-			if( rowWidth == rect.width ) {
-				rect.height++;
+            var bodyDef = new b2.BodyDef();
+            bodyDef.position.Set(
+                rect.x * tilesize * b2.SCALE + rect.width * tilesize / 2 * b2.SCALE,
+                rect.y * tilesize * b2.SCALE + rect.height * tilesize / 2 * b2.SCALE
+            );
 
-				// Unset tile row from the map
-				for( wx = x; wx < x + rect.width; wx++ ) {
-					data[wy][wx] = 0;
-				}
-			}
-			else {
-				return rect;
-			}
-		}
-		return rect;
-	},
+            var body = world.CreateBody( bodyDef );
+            var shape = new b2.PolygonShape();
+            shape.SetAsBox(
+                rect.width * tilesize / 2 * b2.SCALE,
+                rect.height * tilesize / 2 * b2.SCALE
+            );
+            body.CreateFixture2( shape );
+        }
 
-
-	update: function() {
-		// Save found by Alexandre.  Thanks!
-		ig.world.Step( ig.system.tick, 5, 5 );
-		ig.world.ClearForces();
-		this.parent();
-	},
+        return world;
+    },
 
 
-	draw: function() {
-		this.parent();
+    _extractRectFromMap: function( data, width, height, x, y ) {
+        var rect = {x: x, y: y, width: 1, height: 1};
 
-		if( this.debugCollisionRects ) {
-			// Draw outlines of all collision rects
-			var ts = this.collisionMap.tilesize;
-			for( var i = 0; i < this.collisionRects.length; i++ ) {
-				var rect = this.collisionRects[i];
-				ig.system.context.strokeStyle = '#00ff00';
-				ig.system.context.strokeRect(
-					ig.system.getDrawPos( rect.x * ts - this.screen.x ),
-					ig.system.getDrawPos( rect.y * ts - this.screen.y ),
-					ig.system.getDrawPos( rect.width * ts ),
-					ig.system.getDrawPos( rect.height * ts )
-				);
-			}
-		}
-	}
+        // Find the width of this rect
+        for(var wx = x + 1; wx < width && data[y][wx]; wx++ ) {
+            rect.width++;
+            data[y][wx] = 0; // unset tile
+        }
+
+        // Check if the next row with the same width is also completely solid
+        for( var wy = y + 1; wy < height; wy++ ) {
+            var rowWidth = 0;
+            for( wx = x; wx < x + rect.width && data[wy][wx]; wx++ ) {
+                rowWidth++;
+            }
+
+            // Same width as the rect? -> All tiles are solid; increase height
+            // of this rect
+            if( rowWidth == rect.width ) {
+                rect.height++;
+
+                // Unset tile row from the map
+                for( wx = x; wx < x + rect.width; wx++ ) {
+                    data[wy][wx] = 0;
+                }
+            }
+            else {
+                return rect;
+            }
+        }
+        return rect;
+    },
+
+
+    update: function() {
+        // Save found by Alexandre.  Thanks!
+        ig.world.Step( ig.system.tick, 5, 5 );
+        ig.world.ClearForces();
+        this.parent();
+    },
+
+
+    draw: function() {
+        this.parent();
+
+        if( this.debugCollisionRects ) {
+            // Draw outlines of all collision rects
+            var ts = this.collisionMap.tilesize;
+            for( var i = 0; i < this.collisionRects.length; i++ ) {
+                var rect = this.collisionRects[i];
+                ig.system.context.strokeStyle = '#00ff00';
+                ig.system.context.strokeRect(
+                    ig.system.getDrawPos( rect.x * ts - this.screen.x ),
+                    ig.system.getDrawPos( rect.y * ts - this.screen.y ),
+                    ig.system.getDrawPos( rect.width * ts ),
+                    ig.system.getDrawPos( rect.height * ts )
+                );
+            }
+        }
+    }
 
 
 });
