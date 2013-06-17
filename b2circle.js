0cbba1a12f4c7de2f3a3756efda416bfaf445817
Jonathan Commins <joncom@gmail.com>
2013-06-17 15:27:13 -0700

    Formatted with jsFormat.

--- b2circle.js
+++ b2circle.js
@@ -1,4 +1,3 @@
-
 ig.module(
     'plugins.box2d.b2circle'
 )
@@ -9,60 +8,60 @@ ig.module(
 .defines(function(){
 
     ig.Box2DCircle = ig.Entity.extend({
-    body: null,
-    angle: 0,
 
-    init: function( x, y , settings ) {
-        this.parent( x, y, settings );
+        body: null,
+        angle: 0,
 
-        // Only create a box2d body when we are not in Weltmeister
-        if( !ig.global.wm ) {
-        this.createBody();
+        init: function(x, y, settings) {
+            this.parent(x, y, settings);
+
+            // Only create a box2d body when we are not in Weltmeister
+            if (!ig.global.wm) {
+                this.createBody();
+            }
+        },
+
+        createBody: function() {
+            // Create Body
+            var bodyDef = new b2.BodyDef();
+
+            // Set default position
+            bodyDef.position.Set(
+            (this.pos.x + this.radius / 2) * b2.SCALE, (this.pos.y + this.radius / 2) * b2.SCALE);
+
+            // Define Body Attributes
+            bodyDef.type = b2.Body.b2_dynamicBody;
+
+            // Add to World
+            this.body = ig.world.CreateBody(bodyDef);
+
+            // Define Shape
+            var circleDef = new b2.CircleShape();
+            circleDef.SetRadius(this.radius * b2.SCALE);
+
+            // Finally create the fixture
+            this.body.CreateFixture2(circleDef, 0.1);
+        },
+
+        update: function() {
+            var p = this.body.GetPosition();
+            this.pos = {
+                x: (p.x / b2.SCALE - this.radius / 2),
+                y: (p.y / b2.SCALE - this.radius / 2)
+            };
+            this.angle = this.body.GetAngle().round(2);
+
+            if (this.currentAnim) {
+                this.currentAnim.update();
+                this.currentAnim.angle = this.angle;
+            }
+        },
+
+        kill: function() {
+            ig.world.DestroyBody(this.body);
+            this.parent();
         }
-    },
 
-    createBody: function() {
-        // Create Body
-        var bodyDef = new b2.BodyDef();
-
-        // Set default position
-        bodyDef.position.Set(
-        (this.pos.x + this.radius / 2) * b2.SCALE,
-        (this.pos.y + this.radius / 2) * b2.SCALE
-        );
-
-        // Define Body Attributes
-        bodyDef.type = b2.Body.b2_dynamicBody;
-
-        // Add to World
-        this.body = ig.world.CreateBody(bodyDef);
-
-        // Define Shape
-        var circleDef = new b2.CircleShape();
-        circleDef.SetRadius( this.radius * b2.SCALE );
-
-        // Finally create the fixture
-        this.body.CreateFixture2(circleDef, 0.1);
-    },
-
-    update: function() {
-        var p = this.body.GetPosition();
-        this.pos = {
-        x: (p.x / b2.SCALE - this.radius / 2),
-        y: (p.y / b2.SCALE - this.radius / 2 )
-        };
-        this.angle = this.body.GetAngle().round(2);
-
-        if( this.currentAnim ) {
-        this.currentAnim.update();
-        this.currentAnim.angle = this.angle;
-        }
-    },
-
-    kill: function() {
-        ig.world.DestroyBody( this.body );
-        this.parent();
-    }
     });
 
 });
--- b2static.js
+++ b2static.js
@@ -8,57 +8,56 @@ ig.module(
 .defines(function(){
 
     ig.Box2DStatic = ig.Entity.extend({
-    body: null,
-    angle: 0,
 
-    init: function( x, y , settings ) {
-        this.parent( x, y, settings );
+        body: null,
+        angle: 0,
 
-        // Only create a box2d body when we are not in Weltmeister
-        if( !ig.global.wm ) {
-        this.createBody();
-        }
-    },
+        init: function(x, y, settings) {
+            this.parent(x, y, settings);
 
-    createBody: function() {
-        var bodyDef = new b2.BodyDef();
-        bodyDef.position.Set(
-        (this.pos.x + this.size.x / 2) * b2.SCALE,
-        (this.pos.y + this.size.y / 2) * b2.SCALE
-        );
+            // Only create a box2d body when we are not in Weltmeister
+            if (!ig.global.wm) {
+                this.createBody();
+            }
+        },
 
-        bodyDef.type = b2.Body.b2_staticBody;
+        createBody: function() {
+            var bodyDef = new b2.BodyDef();
+            bodyDef.position.Set(
+            (this.pos.x + this.size.x / 2) * b2.SCALE, (this.pos.y + this.size.y / 2) * b2.SCALE);
 
-        this.body = ig.world.CreateBody(bodyDef);
+            bodyDef.type = b2.Body.b2_staticBody;
 
-        var shapeDef = new b2.PolygonShape();
-        shapeDef.SetAsBox(
-        this.size.x / 2 * b2.SCALE,
-        this.size.y / 2 * b2.SCALE
-        );
+            this.body = ig.world.CreateBody(bodyDef);
+
+            var shapeDef = new b2.PolygonShape();
+            shapeDef.SetAsBox(
+                this.size.x / 2 * b2.SCALE,
+                this.size.y / 2 * b2.SCALE);
 
             // CreateFixture2( var shapedef, density )
-        this.body.CreateFixture2(shapeDef, 1.0);
-    },
+            this.body.CreateFixture2(shapeDef, 1.0);
+        },
 
-    update: function() {
-        var p = this.body.GetPosition();
-        this.pos = {
-        x: (p.x / b2.SCALE - this.size.x / 2),
-        y: (p.y / b2.SCALE - this.size.y / 2 )
-        };
-        this.angle = this.body.GetAngle().round(2);
+        update: function() {
+            var p = this.body.GetPosition();
+            this.pos = {
+                x: (p.x / b2.SCALE - this.size.x / 2),
+                y: (p.y / b2.SCALE - this.size.y / 2)
+            };
+            this.angle = this.body.GetAngle().round(2);
 
-        if( this.currentAnim ) {
-        this.currentAnim.update();
-        this.currentAnim.angle = this.angle;
+            if (this.currentAnim) {
+                this.currentAnim.update();
+                this.currentAnim.angle = this.angle;
+            }
+        },
+
+        kill: function() {
+            ig.world.DestroyBody(this.body);
+            this.parent();
         }
-    },
 
-    kill: function() {
-        ig.world.DestroyBody( this.body );
-        this.parent();
-    }
     });
 
 });
