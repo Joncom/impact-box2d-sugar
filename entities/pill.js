d4014acbb5f264ce8fd6a4eb482098caedf55d97
Jonathan Commins <joncom@gmail.com>
2013-06-18 20:59:51 -0700

    Created pill entity.

---
+++ entities/pill.js
@@ -0,0 +1,52 @@
+ig.module('plugins.box2d.entities.pill')
+.requires('plugins.box2d.entity')
+.defines(function(){
+
+    ig.b2Pill = ig.Box2DEntity.extend({
+
+        createBody: function() {
+            var bodyDef = new b2.BodyDef();
+            bodyDef.type = this.bodyType;
+            bodyDef.allowSleep = this.allowSleep;
+            bodyDef.position.Set(
+                (this.pos.x + this.size.x / 2) * b2.SCALE,
+                (this.pos.y + this.size.y / 2) * b2.SCALE
+            );
+            this.body = ig.world.CreateBody(bodyDef);
+
+            var radius = this.size.x / 2;
+            var circleOffsetY = this.size.y / 2 - radius;
+
+            var topCircleDef = new b2.CircleShape();
+            topCircleDef.SetRadius(radius * b2.SCALE);
+            topCircleDef.SetLocalPosition(0, -circleOffsetY * b2.SCALE);
+            topCircleFixture = this.body.CreateFixture2(topCircleDef, this.density);
+            topCircleFixture.SetRestitution(this.bounciness);
+            topCircleFixture.SetFriction(1);
+            topCircleFixture.isSensor = this.isSensor;
+
+            var bottomCircleDef = new b2.CircleShape();
+            bottomCircleDef.SetRadius(radius * b2.SCALE);
+            bottomCircleDef.SetLocalPosition(0, circleOffsetY * b2.SCALE);
+            bottomCircleFixture = this.body.CreateFixture2(bottomCircleDef, this.density);
+            bottomCircleFixture.SetRestitution(this.bounciness);
+            bottomCircleFixture.SetFriction(1);
+            topCircleFixture.isSensor = this.isSensor;
+
+            var boxDef = new b2.PolygonDef();
+            var boxWidth = this.size.x / 2 * b2.SCALE;
+            var boxHeight = (this.size.y / 2 - this.size.x / 2) * b2.SCALE;
+            boxDef.SetAsBox(boxWidth, boxHeight);
+            boxFixture = this.body.CreateFixture2(boxDef, this.density);
+            boxFixture.SetRestitution(this.bounciness);
+            boxFixture.SetFriction(1);
+            topCircleFixture.isSensor = this.isSensor;
+
+            this.body.SetFixedRotation(this.isFixedRotation);
+            this.body.SetBullet(this.isBullet);
+            this.body.CreateFixture(fixtureDef);
+        }
+
+    });
+
+});
