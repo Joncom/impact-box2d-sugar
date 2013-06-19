061d44961d5125650d849246d7b03a4c19f6959a
Jonathan Commins <joncom@gmail.com>
2013-06-18 22:07:28 -0700

    Fixed freeze caused by method expecting different input.

--- entities/pill.js
+++ entities/pill.js
@@ -17,14 +17,14 @@ ig.module('plugins.box2d.entities.pill')
             var circleOffsetY = this.size.y / 2 - radius;
 
             var topCircleDef = new b2.CircleShape(radius * b2.SCALE);
-            topCircleDef.SetLocalPosition(0, -circleOffsetY * b2.SCALE);
+            topCircleDef.SetLocalPosition(new b2.Vec2(0, -circleOffsetY * b2.SCALE));
             topCircleFixture = this.body.CreateFixture2(topCircleDef, this.density);
             topCircleFixture.SetRestitution(this.bounciness);
             topCircleFixture.SetFriction(1);
             topCircleFixture.isSensor = this.isSensor;
 
             var bottomCircleDef = new b2.CircleShape(radius * b2.SCALE);
-            bottomCircleDef.SetLocalPosition(0, circleOffsetY * b2.SCALE);
+            bottomCircleDef.SetLocalPosition(new b2.Vec2(0, circleOffsetY * b2.SCALE));
             bottomCircleFixture = this.body.CreateFixture2(bottomCircleDef, this.density);
             bottomCircleFixture.SetRestitution(this.bounciness);
             bottomCircleFixture.SetFriction(1);
