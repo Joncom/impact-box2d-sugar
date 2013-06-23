e575ca31b7edac285feaa96de4d6e1bbb5737608
Jonathan Commins <joncom@gmail.com>
2013-06-23 02:51:15 -0700

    Pill body shape now supports being more wide than tall.

--- entities/pill.js
+++ entities/pill.js
@@ -13,24 +13,25 @@ ig.module('plugins.box2d.entities.pill')
             );
             this.body = ig.world.CreateBody(bodyDef);
 
-            var radius = this.size.x / 2;
-            var circleOffsetY = this.size.y / 2 - radius;
+            var radius = (this.size.x > this.size.y ? this.size.y / 2 : this.size.x / 2);
+            var circleOffsetX = (this.size.x > this.size.y ? (this.size.x / 2 - radius) : 0);
+            var circleOffsetY = (this.size.y > this.size.x ? (this.size.y / 2 - radius) : 0);
 
-            var topCircleDef = new Box2D.Collision.Shapes.b2CircleShape(radius * Box2D.SCALE);
-            topCircleDef.SetLocalPosition(new Box2D.Common.Math.b2Vec2(0, -circleOffsetY * Box2D.SCALE));
-            topCircleFixture = this.body.CreateFixture2(topCircleDef, this.density);
-            topCircleFixture.SetRestitution(this.bounciness);
-            topCircleFixture.SetFriction(1);
+            var circleADef = new Box2D.Collision.Shapes.b2CircleShape(radius * Box2D.SCALE);
+            circleADef.SetLocalPosition(new Box2D.Common.Math.b2Vec2(-circleOffsetX * Box2D.SCALE, -circleOffsetY * Box2D.SCALE));
+            circleAFixture = this.body.CreateFixture2(circleADef, this.density);
+            circleAFixture.SetRestitution(this.bounciness);
+            circleAFixture.SetFriction(1);
 
-            var bottomCircleDef = new Box2D.Collision.Shapes.b2CircleShape(radius * Box2D.SCALE);
-            bottomCircleDef.SetLocalPosition(new Box2D.Common.Math.b2Vec2(0, circleOffsetY * Box2D.SCALE));
-            bottomCircleFixture = this.body.CreateFixture2(bottomCircleDef, this.density);
-            bottomCircleFixture.SetRestitution(this.bounciness);
-            bottomCircleFixture.SetFriction(1);
+            var circleBDef = new Box2D.Collision.Shapes.b2CircleShape(radius * Box2D.SCALE);
+            circleBDef.SetLocalPosition(new Box2D.Common.Math.b2Vec2(circleOffsetX * Box2D.SCALE, circleOffsetY * Box2D.SCALE));
+            circleBFixture = this.body.CreateFixture2(circleBDef, this.density);
+            circleBFixture.SetRestitution(this.bounciness);
+            circleBFixture.SetFriction(1);
 
             var boxDef = new Box2D.Collision.Shapes.b2PolygonShape();
-            var boxWidth = this.size.x / 2 * Box2D.SCALE;
-            var boxHeight = (this.size.y / 2 - this.size.x / 2) * Box2D.SCALE;
+            var boxWidth = (this.size.x > this.size.y ? this.size.y : this.size.x) / 2 * Box2D.SCALE;
+            var boxHeight = (this.size.x > this.size.y ? this.size.y / 2 : (this.size.y / 2 - this.size.x / 2))  * Box2D.SCALE;
             boxDef.SetAsBox(boxWidth, boxHeight);
             boxFixture = this.body.CreateFixture2(boxDef, this.density);
             boxFixture.SetRestitution(this.bounciness);
