5496358043e34baa580df6aace7a604ed771cf3a
Jonathan Commins <joncom@gmail.com>
2013-06-23 22:49:40 -0700

    Removed redundant entity module.

--- entities/box.js
+++
@@ -1,8 +0,0 @@
-ig.module('plugins.box2d.entities.box')
-.requires('plugins.box2d.entity')
-.defines(function(){
-
-    // Box shape is the defined by default.
-    ig.b2Box = ig.Entity.extend({});
-
-});
